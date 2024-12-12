c7dabfb8e0cc629bcd139e22fdd18319e83765bc
Sujay Ghadge <sujayghadge@gmail.com>
2024-12-12 10:02:04 +0530

    added a funciton to get current time stamp

--- AttendenceLogger.py
+++ AttendenceLogger.py
@@ -1,5 +1,6 @@
 # Description: Class file for the attendence logging part of the app.
 import os 
+from datetime import datetime
 
 class AttendenceLogger:
 
@@ -9,3 +10,11 @@ class AttendenceLogger:
     
     def get_attendance_log_file(self):
         return self.__attendance_log_file
+    
+    def get_current_timestamp(self):
+        """Generate current timestamp."""
+        current_timestamp = datetime.now()
+        date = current_timestamp.date()
+        time = current_timestamp.strftime('%I:%M:%S %p')
+        day_name = current_timestamp.strftime("%A")
+        return {"Date": date, "Time": time, "Day": day_name}
