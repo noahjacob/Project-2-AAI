90c4cd6a8c93bb7d8fd5ef26e17c12347855a8ca
Sujay Ghadge <sujayghadge@gmail.com>
2024-12-12 10:00:36 +0530

    initialized member variables and getter function

--- AttendenceLogger.py
+++ AttendenceLogger.py
@@ -1,4 +1,11 @@
 # Description: Class file for the attendence logging part of the app.
+import os 
 
 class AttendenceLogger:
-    pass
+
+    def __init__(self):
+        self.__attendance_log_file = "attendance_logs/attendance.csv"
+        os.makedirs("attendance_logs", exist_ok=True)
+    
+    def get_attendance_log_file(self):
+        return self.__attendance_log_file
